790fcaae4a0c9e8ef82681b27fadb533f6e5c566
Stephen Michaels <stephen.michaels3333@gmail.com>
2022-10-08 18:03:11 -0400

    initial commit

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to the number guessing game!");
+            Console.WriteLine("Can you guess my favorite number?");
+            Console.WriteLine("What is the upper limit for the range of number?");
+
+            var userResponse = Console.ReadLine();
+            var upperLimit = int.Parse(userResponse);
+
+            var random = new Random();
+            var number = random.Next(1, upperLimit);
+
+            Console.WriteLine("Input your guess:");
+
+            var guess = int.Parse(Console.ReadLine());
+
+            if (guess > number)
+            {
+                Console.WriteLine("Your guess is too high! Try again!");
+            }
+            else if (guess < number)
+            {
+                Console.WriteLine("Your guess is too low! Try again!");
+            }
+            else
+            {
+                Console.WriteLine("Correct");
+            }
         }
     }
 }
